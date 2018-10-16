29d288a9ba6122bf130d8caf61aafda659267348
Vittorio Giovanelli <takky85@gmail.com>
2018-10-16 17:58:49 +0200

    Added NPM configuration reader for PGToken

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,15 @@
 "use strict"
 
 const cdvToPgb = require('../lib/cordovaToPhonegap')
+const pgInterface = require('../lib/phonegapInterface')
 const argv = require('yargs').argv
 const args = process.argv.slice(2)
 
-return cdvToPgb.uploadProject(args[0], argv.t || argv.token, argv.f || argv.folder)
+Promise.resolve()
+.then(async () => {
+  await pgInterface.init()
+  const response = await pgInterface.unlockKey('ios/987808', 'mobimentum')
+  console.log(JSON.stringify(response,null,2))
+})
+
+// return cdvToPgb.uploadProject(args[0], argv.t || argv.token, argv.f || argv.folder)
